6fc2033c51d8c2ec75358c75c8979cac3ea20609
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-24 16:50:16 -0300

    erro02

---
+++ aula07/Erro02.java
@@ -0,0 +1,12 @@
+package aula07;
+
+public class Erro02 {
+    public static void main(String[] args) {
+
+        var int;
+        int cont;
+
+        System.out.println("Erro" + cont);
+    }
+    
+}
