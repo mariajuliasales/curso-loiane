400390a908c279351f013e870b45760c1cecb548
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-24 15:06:57 -0300

    aula06

---
+++ aula06/exercicios06/Argumentos2.java
@@ -0,0 +1,8 @@
+package aula06.exercicios06;
+
+public class Argumentos2 {
+    public static void main(String[] args) {
+        System.out.println("Olá, Mundo!");
+        System.out.println(args[0] + " " + args[1]);
+    }
+}
