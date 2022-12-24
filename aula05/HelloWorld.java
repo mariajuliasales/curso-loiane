0c5b1b82c09f1fbf6b02581ba865b8da4795b310
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-24 13:35:50 -0300

    aula05

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,7 @@
+package aula05;
+public class HelloWorld {
+
+    static public void main(String[] args) {
+        System.out.println("Hello Wolrd");
+    }
+}
---
+++ aula05/exercicios05/OlaMundo.java
@@ -0,0 +1,7 @@
+package aula05.exercicios05;
+
+public class OlaMundo {
+    static public void main(String[] args) {
+        System.out.println("Olá Mundo!");
+    }
+}
