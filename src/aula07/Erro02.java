91f491e5a897d8f67e84b41e898ab92d5bfbdf2f
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-27 16:51:40 -0300

    consertar erro

--- src/aula07/Erro02.java
+++ src/aula07/Erro02.java
@@ -3,10 +3,10 @@ package aula07;
 public class Erro02 {
 
 	public static void main(String[] args) {
-        var int;
+        int erro;
         int cont;
 
-        System.out.println("Erro" + cont);
+        System.out.println("Erro");
 	}
 
 }
