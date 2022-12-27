597d790846d37bdf3006cd845f5e0a73e8aa35b9
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-27 16:56:54 -0300

    aula10

--- src/aula07/Erro02.java
+++ src/aula07/Erro02.java
@@ -3,10 +3,10 @@ package aula07;
 public class Erro02 {
 
 	public static void main(String[] args) {
-        int erro;
-        int cont;
+		int erro;
+		int cont;
 
-        System.out.println("Erro");
+		System.out.println("Erro");
 	}
 
 }
---
+++ src/mariajulia/cursojava/aula10/Variaveis.java
@@ -0,0 +1,19 @@
+package mariajulia.cursojava.aula10;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+
+		int idade = 20;
+		String nome = "Maria Julia Sales";
+		String nomeCachorro = "Mel";
+		char machoOuFemea = 'F';
+
+		System.out.println("Meu nome " + nome);
+		System.out.println("Minha idade: " + idade);
+		System.out.println("Nome da minha cachorra: " + nomeCachorro);
+		System.out.println("Macho ou femea: " + machoOuFemea);
+
+	}
+
+}
