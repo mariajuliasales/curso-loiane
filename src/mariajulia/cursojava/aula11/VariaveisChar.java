07228923983b576a9a4eae70e9658a9967c37a6c
mariajuliasales <salesmariajulia11@gmail.com>
2022-12-27 21:08:23 -0300

    aula11

---
+++ src/mariajulia/cursojava/aula11/VariaveisChar.java
@@ -0,0 +1,20 @@
+package mariajulia.cursojava.aula11;
+
+public class VariaveisChar {
+
+	public static void main(String[] args) {
+		
+		char a = 77;
+		char b = 97;
+		char c = 114;
+		char d = 105;
+		char e = 74;
+		char f = 117;
+		char g = 108;
+
+		System.out.println(" " + a + b + c + d + b + " " + e + f + g + d + b);
+		System.out.println(a + b + c + d + b + e + f + g + d + b);
+
+	}
+
+}
---
+++ src/mariajulia/cursojava/aula11/VariaveisPontoFlutuante.java
@@ -0,0 +1,15 @@
+package mariajulia.cursojava.aula11;
+
+public class VariaveisPontoFlutuante {
+
+	public static void main(String[] args) {
+
+		double valorPassagem = 2.977;
+		float valorTomate = 3.95f;
+
+		System.out.println("Valor da passagem: R$" + valorPassagem);
+		System.out.println("Valor do tomate: R$" + valorTomate);
+
+	}
+
+}
