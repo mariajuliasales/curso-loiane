90d0fbd4679f707537567294febb9b2b9a8e819a
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-01 21:41:46 -0300

    aula 15

---
+++ src/mariajulia/cursojava/aula15/SwitchCase.java
@@ -0,0 +1,61 @@
+package mariajulia.cursojava.aula15;
+
+import java.util.Scanner;
+
+public class SwitchCase {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Digite um numero inteiro.");
+		int diaSemana = scan.nextInt();
+
+		if (diaSemana == 1) {
+			System.out.println("Domingo");
+		} else if (diaSemana == 2) {
+			System.out.println("Segunda");
+		} else if (diaSemana == 3) {
+			System.out.println("Terça");
+		} else if (diaSemana == 4) {
+			System.out.println("Quarta");
+		} else if (diaSemana == 5) {
+			System.out.println("Quinta");
+		} else if (diaSemana == 6) {
+			System.out.println("Sexta");
+		} else if (diaSemana == 1) {
+			System.out.println("ábado");
+		} else {
+			System.out.println("Não é um dia da semana valido.");
+		}
+
+		switch (diaSemana) {
+		case 1:
+			System.out.println("Domingo");
+			break;
+		case 2:
+			System.out.println("Segunda");
+			break;
+		case 3:
+			System.out.println("Terça");
+			break;
+		case 4:
+			System.out.println("Quarta");
+			break;
+		case 5:
+			System.out.println("Quinta");
+			break;
+		case 6:
+			System.out.println("Sexta");
+			break;
+		case 7:
+			System.out.println("Sábado");
+			break;
+		default:
+			System.out.println("Não é um dia da semana valido.");
+			;
+		}
+
+	}
+
+}
