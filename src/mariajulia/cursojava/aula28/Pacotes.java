dcf0b34d3748ce44048aedf09f8985adc9e6c0e8
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-04 21:34:02 -0300

    aula 28

---
+++ src/mariajulia/cursojava/aula28/Pacotes.java
@@ -0,0 +1,20 @@
+package mariajulia.cursojava.aula28;
+
+import mariajulia.cursojava.aula13.labs.Exercicio05;
+import mariajulia.cursojava.aula15.labs.Exercicio06;
+
+import mariajulia.cursojava.aula27.Carro;
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+
+		Carro carro;
+		mariajulia.cursojava.aula26.Carro carro2;
+
+		Exercicio05 ex05;
+		Exercicio06 ex06;
+
+	}
+
+}
