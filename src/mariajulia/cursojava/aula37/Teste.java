67e67f8116b59f6e093e12a62473d32cee514bd5
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-06 15:05:21 -0300

    aula 37

---
+++ src/mariajulia/cursojava/aula37/Teste.java
@@ -0,0 +1,25 @@
+package mariajulia.cursojava.aula37;
+
+public class Teste {
+
+	public static void main(String[] args) {
+/*
+		Aluno aluno = new Aluno();
+
+		Professor professor = new Professor();
+
+		professor.setNome("Maria Julia");
+
+		aluno.setNome("Julia Maria");
+
+		System.out.println(professor.getNome());
+		System.out.println(aluno.getNome());
+		
+*/
+		Pessoa aluno = new Aluno();
+		
+		Pessoa professor = new Professor();
+	
+	}
+
+}
