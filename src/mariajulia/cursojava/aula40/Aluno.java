42ddaa42b3bee36eeb2577a6424ad507e0b237c4
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-06 15:05:42 -0300

    aula 40

---
+++ src/mariajulia/cursojava/aula40/Aluno.java
@@ -0,0 +1,53 @@
+package mariajulia.cursojava.aula40;
+
+public class Aluno extends Pessoa {
+
+	private String curso;
+	private double[] notas;
+
+	public Aluno() {
+		super();
+	}
+
+	public Aluno(String nome, String endereco, String telefone, String curso) {
+		super(nome, endereco, telefone);
+		this.curso = curso;
+	}
+
+	public String getCursos() {
+		return curso;
+	}
+
+	public void setCursos(String cursos) {
+		this.curso = cursos;
+	}
+
+	public double[] getNotas() {
+		return notas;
+	}
+
+	public void setNotas(double[] notas) {
+		this.notas = notas;
+	}
+
+	public double calcularMedia() {
+		return 0;
+	}
+
+	public boolean verificarAprovado() {
+		return true;
+	}
+
+	public void metodoQualquer() {
+		super.setTelefoneCelular("5566556600");
+
+		this.setTelefoneCelular("20202020");
+	}
+
+	public String obterEtiquetaEndereco() {
+		String s = "Endereço do Aluno: ";
+		s += super.getEndereco();
+
+		return s;
+	}
+}
---
+++ src/mariajulia/cursojava/aula40/Teste.java
@@ -0,0 +1,24 @@
+package mariajulia.cursojava.aula40;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+		Pessoa aluno = new Aluno();
+		Pessoa professor = new Professor();
+		
+		Aluno aluno2 = new Aluno();
+
+		pessoa.setEndereco("Rua 1, num 1");
+		aluno.setEndereco("Rua 2, num 2");
+		professor.setEndereco("Rua 3, num 3");
+		
+		System.out.println(pessoa.obterEtiquetaEndereco());
+		System.out.println(aluno.obterEtiquetaEndereco());
+		System.out.println(professor.obterEtiquetaEndereco());
+		System.out.println(aluno2.obterEtiquetaEndereco());
+
+	}
+
+}
