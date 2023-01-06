87299198516b88825152ece95d1daa93982f3881
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-06 15:05:33 -0300

    aula 39

---
+++ src/mariajulia/cursojava/aula39/Teste.java
@@ -0,0 +1,18 @@
+package mariajulia.cursojava.aula39;
+
+import mariajulia.cursojava.aula39.teste.Aluno;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Aluno aluno = new Aluno();
+
+		Pessoa pessoa = new Pessoa();
+
+		aluno.nomeVisibilidade = "juju";
+
+		pessoa.nomeVisibilidade = "";
+	}
+
+}
---
+++ src/mariajulia/cursojava/aula39/teste/TesteAcesso.java
@@ -0,0 +1,16 @@
+package mariajulia.cursojava.aula39.teste;
+
+import mariajulia.cursojava.aula39.Pessoa;
+
+public class TesteAcesso {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+
+//		pessoa.nomeVisibilidade = "juju";
+//		Não consigo acessar pois é protected, apenas a classe, pacote e herdeiros podem acessar
+
+	}
+
+}
