4a9886f24b05c3d598c94c884e11f06e7b1ad17c
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-07 01:10:49 -0300

    aula 42

---
+++ src/mariajulia/cursojava/aula42/Constantes.java
@@ -0,0 +1,8 @@
+package mariajulia.cursojava.aula42;
+
+public final class Constantes {
+
+	public static final String URL_GITHUB = "https://github.com/mariajuliasales";
+	public static final String URL_CURSO = "https://github.com/mariajuliasales/curso-loiane";
+
+}
---
+++ src/mariajulia/cursojava/aula42/Teste.java
@@ -0,0 +1,20 @@
+package mariajulia.cursojava.aula42;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		Pessoa pessoa = new Pessoa();
+//		Pessoa aluno = new Aluno();
+//		Pessoa professor = new Professor();
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+
+		System.out.print("Minha página no Github: ");
+		System.out.println(Constantes.URL_GITHUB);
+		System.out.print("Página do curso Java no Github: ");
+		System.out.println(Constantes.URL_CURSO);
+
+	}
+
+}
