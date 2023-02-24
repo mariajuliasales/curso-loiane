1b01f3b62fdd1b2bb6e18403e99baaed27627d81
mariajuliasales <salesmariajulia11@gmail.com>
2023-02-24 19:20:11 -0300

    aula 62

---
+++ src/mariajulia/cursojava02/aula62/ExemproVarArgs.java
@@ -0,0 +1,41 @@
+package mariajulia.cursojava02.aula62;
+
+public class ExemproVarArgs {
+	public static void main(String[] args) {
+
+		System.out.println(soma(2, 3, 4));
+		System.out.println(soma(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+	}
+
+	static int soma(int a, int b) {
+		return a + b;
+	}
+
+	static int soma(int a, int b, int c) {
+		return a + b + c;
+	}
+
+	static int soma(int[] vetor) {
+
+		int total = 0;
+
+		for (int i = 0; i < vetor.length; i++) {
+			total += vetor[i];
+		}
+
+		return total;
+	}
+
+	static int soma(Integer... vetor) {
+
+		int total = 0;
+
+		for (int i = 0; i < vetor.length; i++) {
+			total += vetor[i];
+		}
+
+		return total;
+	}
+
+}
