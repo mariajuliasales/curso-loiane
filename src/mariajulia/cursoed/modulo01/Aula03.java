b3e97d83f3149a23d0508e1561400cbde2533f9c
mariajuliasales <salesmariajulia11@gmail.com>
2024-04-30 10:40:39 -0300

    m01 aula 3: adicionar elemento ao final do vetor

--- src/mariajulia/cursoed/modulo01/Aula03.java
+++ src/mariajulia/cursoed/modulo01/Aula03.java
@@ -4,12 +4,20 @@ public class Aula03 {
 
 	public static void main(String[] args) {
 
-		Vetor novoVetor = new Vetor(5);
+		Vetor novoVetor = new Vetor(2);
+/*		
+		try {
+			novoVetor.adicionarElemento("elemento 01");
+			novoVetor.adicionarElemento("elemento 02");
+			novoVetor.adicionarElemento("elemento 03");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+*/
+		System.out.println(novoVetor.adicionar("elemento 01"));
+		System.out.println(novoVetor.adicionar("elemento 02"));
+		System.out.println(novoVetor.adicionar("elemento 03"));
 		
-		novoVetor.adicionarElemento("elemento 01");
-		
-		novoVetor.adicionarElemento("elemento 02");
-
 	}
 
 }
--- src/mariajulia/cursoed/modulo01/Vetor.java
+++ src/mariajulia/cursoed/modulo01/Vetor.java
@@ -3,11 +3,13 @@ package mariajulia.cursoed.modulo01;
 public class Vetor {
 	
 	private String[] elementos;
+	private int tamanho;
 	
 	public Vetor(int capacidade) {
 		this.elementos = new String[capacidade];
+		this.tamanho = 0;
 	}
-	
+/*	
 	public void adicionarElemento(String elemento) {
 		for(int i = 0; i < this.elementos.length; i++) {
 			if (this.elementos[i] == null) {
@@ -17,5 +19,27 @@ public class Vetor {
 		}
 		
 	}
+*/	
+/*
+	public void adicionarElemento(String elemento) throws Exception {
+		
+		if(this.tamanho<this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;			
+		} else {
+			throw new Exception("Vetor está cheio, não é possível adicionar elemento.");
+		}
 
+	}
+*/	
+	public boolean adicionar(String elemento) {
+		if(this.tamanho<this.elementos.length) {
+			this.elementos[this.tamanho] = elemento;
+			this.tamanho++;	
+			return true;
+		}
+		
+		return false;
+
+	}
 }
